Corrected ordering of vesicle leakage decays and collation so a unique id is assigned to each vesicle

Analyser.py
@@ -438,7 +438,7 @@ class Analyser(object):
         markers = markers +1
         markers[unknown==255] = 0
         clip = clip.reshape(31,31)
-        clip = smooth(clip,sigma = 1)
+        clip = smooth(clip,sigma =1)
         
 
         nclip = cv2.normalize(src=clip,dst=None,alpha = 0,beta = 255,norm_type = cv2.NORM_MINMAX,dtype = cv2.CV_8UC1)
@@ -519,7 +519,7 @@ class Analyser(object):
                 
                         self.firstareatrace[str(label)].append(eccentricity)
                         #self.areatrace[str(label)].append(self.extract_area(testclip,kernel,clip))
-                        self.filtered_areatrace[str(label)] = smooth(self.firstareatrace[str(label)],0.5)
+                        self.filtered_areatrace[str(label)] = smooth(self.firstareatrace[str(label)],3)
         
                         self.secondintensitytrace[str(label)].append(np.average(img[img > 0]))
                     except KeyError:
@@ -528,7 +528,7 @@ class Analyser(object):
                 
                         self.firstareatrace[str(label)]=[eccentricity]                       
                         #self.areatrace[str(label)] = [self.extract_area(testclip,kernel,clip)]
-                        self.filtered_areatrace[str(label)]=smooth(self.firstareatrace[str(label)],0.5)
+                        self.filtered_areatrace[str(label)]=smooth(self.firstareatrace[str(label)],3)
                         self.secondintensitytrace[str(label)] = [np.average(img[img>0])]
         
         
@@ -607,7 +607,7 @@ class Analyser(object):
                 
                 self.firstareatrace[str(label)].append(eccentricity)            
                 self.areatrace[str(label)].append(self.extract_area(testclip,kernel,firstclip))
-                self.filtered_firstareatrace[str(label)] = smooth(self.firstareatrace[str(label)],0.5)
+                self.filtered_firstareatrace[str(label)] = smooth(self.firstareatrace[str(label)],3)
 
                 self.firstsecondintensitytrace[str(label)].append(np.average(img[img > 0]))
             except KeyError:
@@ -616,7 +616,7 @@ class Analyser(object):
                 self.areatrace[str(label)] = [self.extract_area(testclip,kernel,firstclip)]
                 self.firstareatrace[str(label)]= [eccentricity]
                 
-                self.filtered_firstareatrace[str(label)]=smooth(self.firstareatrace[str(label)],0.5)
+                self.filtered_firstareatrace[str(label)]=smooth(self.firstareatrace[str(label)],3)
                 self.firstsecondintensitytrace[str(label)] = [np.average(img[img>0])]
 
 
@@ -734,12 +734,12 @@ class Analyser(object):
 
             
             self.firstareatrace[str(label)].append(eccentricity)
-            self.filtered_firstareatrace[str(label)] = smooth(self.firstareatrace[str(label)],0.5)
+            self.filtered_firstareatrace[str(label)] = smooth(self.firstareatrace[str(label)],3)
 
             self.firstsecondintensitytrace[str(label)].append(np.average(img[img > 0]))
         except KeyError:
             self.firstareatrace[str(label)] = [eccentricity]
-            self.filtered_firstareatrace[str(label)]=smooth(self.firstareatrace[str(label)],0.5)
+            self.filtered_firstareatrace[str(label)]=smooth(self.firstareatrace[str(label)],3)
             self.firstsecondintensitytrace[str(label)] = [np.average(img[img>0])]
 
     def delete_vesicle(self,label_as_str):
@@ -831,7 +831,7 @@ class Analyser(object):
         self.ls = livestream(qnd,video[t0:tmax],annotations_on = annotations,annotate_coords = self.centres[str(label)])
 
 
-    def subtract_background(self,sigma = 0.5):
+    def subtract_background(self,sigma = 3):
 
         for key in self.firstintensitytrace.keys():
 

AnalyserGUI.py
@@ -274,7 +274,9 @@ class AnalyserPanel(QWidget):
                 continue
             
             os.chdir(self.network_dir)
-            self.get_traps()
+            traps, labels = self.get_traps()
+            if traps.shape[0] == 0:
+                continue
             
 
                         
@@ -282,7 +284,7 @@ class AnalyserPanel(QWidget):
             t0 = int(self.AControl.t0selector.currentText()) - self.start_offset
             self.AControl.t0selector.setCurrentText(str(t0))
             
-            tmax = t0 +1200
+            tmax = t0 +600
             if tmax > self.analyser.videolength:
                 self.AControl.tmaxselector.setCurrentText(str(self.analyser.videolength - 1))
             else:
@@ -1003,24 +1005,31 @@ class AnalyserPanel(QWidget):
         
         else:
             t0 = self.bgf.peak_max_arg
-            
+            if t0 == 0:
+                print('t0 is 0 so automatic detection of increase in background intensity has failed')
+                
             print(t0 ,'is equal to the peak max index')
             
             self.AControl.t0selector.setCurrentText(str(self.bgf.peak_max_arg))
             file = open(os.getcwd() +'/Experimentalflowrates.csv','w')
-
-            flow_rate = np.average(self.bgf.gradient[int(self.bgf.peak_begin_frame):int(2*self.bgf.peak_max_arg - self.bgf.peak_begin_frame)])
             
+            if self.bgf.peak_begin_frame:
+                
+                flow_rate = np.average(self.bgf.gradient[int(self.bgf.peak_begin_frame):int(2*self.bgf.peak_max_arg - self.bgf.peak_begin_frame)])
+                file.write(self.analyser.videopath + str(flow_rate)+',')
             
 
-            file.write(self.analyser.videopath + str(flow_rate)+',')
             
-
+            else:
+                print("Can't determine flow rate as background intensity rise has not been found")
+            
             file.close()
+
         self.AControl.update()
         self.AControl.override_warning.rejected.connect(self.no_manual_override)
         
-        
+        traps = np.array([])
+        labels = np.array([])
         if self.has_frames:
             try:
                 if multividflag:
@@ -1043,7 +1052,7 @@ class AnalyserPanel(QWidget):
                 else:
                     self.msgbox.setText('Getting traps failed. Check Kernel, and that the visibility of the traps in the\n last frame is good')
                     self.msgbox.show()
-        return t0
+        return traps, labels
             
 
     def create_persisting_times(self):

BackgroundFinder.py
@@ -38,9 +38,9 @@ class BackgroundFinder(object):
         #the peak value of this largest peak is taken as the time at which the concentration of the drug is half of its final concentration
         
         
-        peak_max_val = -1
-        peak_max_arg = 0
-
+        self.peak_max_val = -1
+        self.peak_max_arg = 0
+        self.peak_begin_frame = 0
         for i in range(5,30,5):
             tic = time.time()
             gradient = gaussian(self.gradient[:-100],i)
@@ -50,9 +50,9 @@ class BackgroundFinder(object):
             
             new_peak_arg = np.argmax(gradient)
             
-            if new_peak_val > peak_max_val:
-                peak_max_val = new_peak_val
-                peak_max_arg = new_peak_arg
+            if new_peak_val > self.peak_max_val:
+                self.peak_max_val = new_peak_val
+                self.peak_max_arg = new_peak_arg
                 self.width = i
             toc = time.time()
            
@@ -62,7 +62,7 @@ class BackgroundFinder(object):
         
         
         delta = 0
-        grad_peak2firstval = self.gradient[:peak_max_arg][::-1]
+        grad_peak2firstval = self.gradient[:self.peak_max_arg][::-1]
         
         for num in range(grad_peak2firstval.shape[0]-1):
             newdelta = grad_peak2firstval[num+1]-grad_peak2firstval[num]
@@ -73,8 +73,8 @@ class BackgroundFinder(object):
                 
                 firstmin = num
 
-                self.peak_begin_frame = peak_max_arg - firstmin
-                self.peak_max_arg = peak_max_arg
+                self.peak_begin_frame = self.peak_max_arg - firstmin
+                
                 
                 return 
 

orderforheatmap.py
@@ -10,7 +10,7 @@ import numpy as np
 import pandas as pd
 import sys
 import os
-
+import time
 
 
 
@@ -37,14 +37,68 @@ if __name__ == '__main__':
     
     labels = np.array(list(df.columns))
     data = df.to_numpy(float)
+    columns = data[:,0]
+    print(data.shape)
+    
+    data = data[:,1:]
+    labels = labels[1:]
+    
 
-    print(labels)
-    lifetimes = np.sum(data > 0.5,axis = 0)
+    #lifetimes = np.sum(data > 0.5,axis = 0)
+    lifetimes = []
+    for j in range(0,data.shape[1]):
+        row = data[:,j]
+        lessthanhalf = np.nonzero(row < 0.5)
+        
+        if lessthanhalf[0].shape[0] ==0:
+            lengthofdata = row.shape[0]
+        else:    
+            lengthofdata = lessthanhalf[0][0]
+        
+        lifetimes.append(lengthofdata)
+
+
+    lifetimes = np.array(lifetimes)
+
+
+
     
-    data = np.vstack((labels[np.argsort(lifetimes)],data[:,np.argsort(lifetimes)]))
+    #delete traces where a vesicle has reappeared in the view of the intensity extractor, after the initial vesicle has burst/moved
+    indices_to_remove = []
+    for i in range(0,data.shape[1]):
+        if lifetimes[i] == data[:,i].shape[0]:
+            continue
+        
+        expected_lessthan_half_I = data[lifetimes[i]:,i]
+        #remove nan values
+        expected_lessthan_half_I = expected_lessthan_half_I[np.isnan(expected_lessthan_half_I) == False]
+        
+        #look for presence of intensity values above 0.5 intensity in the region of the trace expected to be lower than half intensity, if we find values greater than 0.5 we delete that row
+        if np.sum(expected_lessthan_half_I[5:] > 0.5) !=0:
+            indices_to_remove.append(i)
+
+    
+    #delete the rows which are suspected to have had a second vesicle appear after the first disappears
+    indices_to_remove = np.array(indices_to_remove)
+    #all these indices are offest from the first row of data which does not count the frame numbers
+
+    lifetimes = np.delete(lifetimes,indices_to_remove)
+
     
     
-    df2 = pd.DataFrame(data[1:],columns = data[0])
+    
+    data = np.delete(data,indices_to_remove,axis = 1)   
+    
+    labels = np.delete(labels,indices_to_remove)
+    
+    data = np.vstack((labels[np.argsort(lifetimes)[::-1]],data[:,np.argsort(lifetimes)][::-1])) 
+    columns = np.concatenate((['frame'],columns))   
+    print(columns.shape)
+    print(data.shape)        
+    data = np.hstack((columns.reshape((columns.shape[0],1)),data))
+    
+        
+    df2 = pd.DataFrame(data)
     labels = ['Unnamed: 0','Unnamed: 0.1']
     try:
         df2 = df2.drop(labels, axis = 1)

trapanalysis.py
@@ -63,7 +63,7 @@ class TrapGetter(object):
         print( mask[mask!=0].shape[0],mask[mask==0].shape[0] )
         if mask[mask!=0].shape[0] > mask[mask==0].shape[0]:
             self.threshold = np.average(frame)+ 4*np.std(frame)
-            
+            print('Otsu threshold failed, has been overriden')
         print(self.threshold)
         mask = np.zeros_like(frame)
         mask[frame > self.threshold] = 1